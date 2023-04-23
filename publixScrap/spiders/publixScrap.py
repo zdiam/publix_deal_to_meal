8508e04d6f2f0de8f6f39cde4bd19cadd467281f
Zach <120227829+zdiam@users.noreply.github.com>
2023-04-23 14:45:26 -0400

    Add files via upload

--- publixScrap/spiders/publixScrap.py
+++ publixScrap/spiders/publixScrap.py
@@ -4,6 +4,8 @@ from items import PublixscrapItem
 from items import PublixscrapType
 
 
+
+
 class publixSpider(scrapy.Spider):
     
     name = 'publix'
@@ -84,85 +86,45 @@ class publixUrl(scrapy.Spider):
     
     name = 'publixUrl'
 
-
- 
     start_urls = [
-        'https://accessibleweeklyad.publix.com/PublixAccessibility/Entry/LandingContent?storeid=2501005&sneakpeek=N&listingid=0'
-    ]
-
-    listing = [
-        '.leftcolumn .listing::attr(href)',
+         'https://accessibleweeklyad.publix.com/PublixAccessibility/Entry/LandingContent?storeid=2501005&sneakpeek=N&listingid=0'
     ]
 
 
-    def start_requests(self):
-        for url in self.start_urls:
-            yield scrapy.Request(url, callback=self.parse_recurse, cb_kwargs={"listing": self.listing})
 
 
-
-    def parse_recurse(self, response, listing):
-
-
-        first = listing[0]
-        rest  = listing[1:]
-        
-        links = response.css(first).extract()
-        
-        if rest:
-            for link in links:
-                yield response.follow(link, callback=self.parse_recurse, cb_kwargs={"listing": rest})
-        else:
-
-            for link in links:
-                yield response.follow(link, callback=self.parse)
-
-
-
-  
     def parse(self, response, dont_filter = True):
 
- 
-
-
-        allItems = response.css('#TitleBar .fl')
+        allItems = response.css('.leftcolumn .listing')
         items = PublixscrapType()
 
         for publix in allItems:
+        
+  
 
 
+            url = publix.css('::attr(href)').extract()
 
+            dealT = publix.css('*::text')
 
-           
-            dealT = publix.css('.bold').extract_first()
-            
-
-
-            
-            
-            items['url'] = response.request.url
-            
-
-
- 
+            items['url'] = url
 
             items['dealT'] = dealT
-
-
-           
-
-        
 
             yield items
 
 
+
+
 process = CrawlerProcess(settings={
     "FEEDS": {
-        "items5.csv": {"format": "csv"},
+        "items7.csv": {"format": "csv"},
     },
 })
 
-process.crawl(publixSpider)
+
+
+# process.crawl(publixSpider)
 process.crawl(publixUrl)
 process.start()
 
